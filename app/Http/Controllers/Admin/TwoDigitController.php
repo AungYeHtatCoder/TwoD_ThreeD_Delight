e2fba209377804ad6efbaee4a99802b9f19a3dea
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-10-18 11:57:38 +0630

    auto delete add

--- app/Http/Controllers/Admin/TwoDigitController.php
+++ app/Http/Controllers/Admin/TwoDigitController.php
@@ -42,6 +42,8 @@ class TwoDigitController extends Controller
         'user_id' => 'required|exists:users,id',
     ]);
 
+    $currentSession = date('H') < 12 ? 'morning' : 'evening';  // before 1 pm is morning
+
     DB::beginTransaction();
 
     try {
@@ -61,12 +63,15 @@ class TwoDigitController extends Controller
             'pay_amount' => $request->totalAmount,
             'total_amount' => $request->totalAmount,
             'user_id' => $request->user_id,
+            'session' => $currentSession
         ]);
 
         $attachData = [];
         foreach($request->amounts as $two_digit_id => $sub_amount) {
             $totalBetAmountForTwoDigit = DB::table('lottery_two_digit_pivot')
+                    ->join('lotteries', 'lotteries.id', '=', 'lottery_two_digit_pivot.lottery_id')
                     ->where('two_digit_id', $two_digit_id)
+                    ->where('lotteries.session', $currentSession)
                     ->sum('sub_amount');
 
             if($totalBetAmountForTwoDigit + $sub_amount > 5000) {
@@ -86,6 +91,61 @@ class TwoDigitController extends Controller
         return redirect()->back()->with('error', $e->getMessage());
     }
 }
+
+//     public function store(Request $request)
+// {
+//     $validatedData = $request->validate([
+//         'selected_digits' => 'required|string',
+//         'amounts' => 'required|array',
+//         'amounts.*' => 'required|integer|min:100|max:5000',
+//         'totalAmount' => 'required|integer|min:100',
+//         'user_id' => 'required|exists:users,id',
+//     ]);
+
+//     DB::beginTransaction();
+
+//     try {
+//         // Deduct the total amount from the user's balance
+//         $user = Auth::user();
+//         $user->balance -= $request->totalAmount;
+
+//         // Check if user balance is negative after deduction
+//         if ($user->balance < 0) {
+//             throw new \Exception('Your balance is not enough.');
+//         }
+
+//         // Update user balance in the database
+//         $user->save();
+
+//         $lottery = Lottery::create([
+//             'pay_amount' => $request->totalAmount,
+//             'total_amount' => $request->totalAmount,
+//             'user_id' => $request->user_id,
+//         ]);
+
+//         $attachData = [];
+//         foreach($request->amounts as $two_digit_id => $sub_amount) {
+//             $totalBetAmountForTwoDigit = DB::table('lottery_two_digit_pivot')
+//                     ->where('two_digit_id', $two_digit_id)
+//                     ->sum('sub_amount');
+
+//             if($totalBetAmountForTwoDigit + $sub_amount > 5000) {
+//                 $twoDigit = TwoDigit::find($two_digit_id);
+//                 throw new \Exception("The two-digit's amount limit for {$twoDigit->two_digit} is full.");
+//             }
+//             $attachData[$two_digit_id] = ['sub_amount' => $sub_amount];
+//         }
+
+//         $lottery->twoDigits()->attach($attachData);
+
+//         DB::commit();
+
+//         return redirect()->back()->with('message', 'Data stored successfully!');
+//     } catch (\Exception $e) {
+//         DB::rollback();
+//         return redirect()->back()->with('error', $e->getMessage());
+//     }
+// }
 
 //     public function store(Request $request)
 // {
